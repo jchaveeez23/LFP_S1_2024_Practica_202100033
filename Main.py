e678fb642c6fc90cc27200995944ce9d1a2d3b50
Josue Chavez <3921213790101@ingenieria.usac.edu.gt>
2024-02-24 15:58:35 -0600

    Me falta jugar y el graphviz

--- Main.py
+++ Main.py
@@ -45,4 +45,87 @@ def PetManager():
                 pets_result = pets_result + "[%s/%s/%s"%(fecha.day,fecha.month,fecha.year) + " %s:%s"%(fecha.hour,fecha.minute)+"] Se creo el gato " + nombre +"\n"
             else:
                 pets_result = "[%s/%s/%s"%(fecha.day,fecha.month,fecha.year) + " %s:%s"%(fecha.hour,fecha.minute)+"] Se creo el gato " + nombre + "\n"
-            
+        elif instruccion[0] == "Dar_de_Comer":
+            ar1 = instruccion[1].split(",")
+            nick = ar1[0].strip('"')
+            for animal in pets:
+                if animal.nombre == nick:
+                    if pets_result != " ":
+                        pets_result = pets_result + animal.Comer(int(ar1[1])) + "\n"
+                    else:
+                        pets_result = pets_result +  animal.comer(int(ar1[1])) + "\n" 
+        elif instruccion[0] == "Resumen_Mascota":
+            ar1 = instruccion[1].split(",")
+            nick = ar1[0].strip('"')
+            for animal in pets:
+                if animal.nombre == nick:
+                    if pets_result != " ":
+                        pets_result = pets_result + animal.Resumen() + "\n"
+                    else:
+                        pets_result = pets_result + animal.Resumen() + "\n"
+        elif linea == "Resumen Global":
+            fecha = datetime.datetime.now()
+            if pets_result != " ":
+                pets_result = pets_result + "[%s/%s/%s"%(fecha.day,fecha.month,fecha.year) + " %s:%s"%(fecha.hour,fecha.minute)+"] " + "----------------- Resumen global -----------------\n"
+            else:
+                pets_result = "[%s/%s/%s"%(fecha.day,fecha.month,fecha.year) + " %s:%s"%(fecha.hour,fecha.minute)+"] " + "----------------- Resumen global -----------------"
+
+            for animal in pets:
+                if pets_result != " ":
+                    pets_result = pets_result + animal.Resumen() + "\n"
+                else:
+                    pets_result = pets_result + animal.F() + "\n"
+            pets_result = pets_result + "[%s/%s/%s"%(fecha.day,fecha.month,fecha.year) + " %s:%s"%(fecha.hour,fecha.minute)+"] " + "--------------------------------------------------\n"
+    #Creacion de archivo
+    resultado = open("C:/Users/cokei/OneDrive/Escritorio/archivo.petworld_result","w")
+    resultado.write(pets_result)
+    resultado.close()
+    os.system("C:/Users/cokei/OneDrive/Escritorio/archivo.petworld_result")
+    menuPrincipal()
+
+def menuPrincipal():
+    os.system("cls")
+    print(" ")
+    print("------Menu Principal-------")
+    print("|                         |")
+    print("| 1. Modulo Pet Manager |")
+    print("| 0. Salir                |")
+    print("|                         |")
+    print("---------------------------")
+    print(" ")
+    while True:
+        lectura = input('Presione el numero de la accion a realizar: ')
+        if lectura.isdigit() == True:
+            lectura = int(lectura)
+            if lectura == 1:
+                modulopetmanager()
+            elif lectura == 0:
+                print("Hasta la proxima c:")
+                exit(0)
+            else:
+                print("\nIngrese una opcion valida\n")
+        else:
+            print("\nIngrese una opcion valida \n")
+
+def modulopetmanager():
+    os.system("cls")
+    print(" ")
+    print("-------Modulo Pet Manager-------")
+    print("|                                |")
+    print("| 1. Carga del archivo .petmanager |")
+    print("| 0. Regresar al menu principal  |")
+    print("|                                |")
+    print("----------------------------------")
+    print(" ")
+    while True:
+        lectura = input('Presione el numero de la accion a realizar: ')
+        if lectura.isdigit() == True:
+            lectura = int(lectura)
+            if lectura == 1:
+                PetManager()
+            elif lectura == 0:
+                menuPrincipal()
+            else:
+                print("\nIngrese una opcion valida \n")
+        else:
+            print("\nIngrese una opcion valida \n")
